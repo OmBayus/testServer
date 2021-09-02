245b3d04e6720b4b6d0fc6973b7889d26a4d531d
ombayus <>
2021-09-02 13:36:49 +0300

    test

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const mongoose = require('mongoose');
+
+const cors = require("cors")
+
+const app = express()
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+const Person = mongoose.model('Person', { name: String });
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    Person.find({}, function (err, data) {
+        res.json(data)
+    });
+    
+})
+
+app.post("/add",(req,res)=>{
+
+    const person = new Person({ name: req.body.name });
+    person.save().then(data => res.json(data));
+    
+})
+
+app.listen(4000)
